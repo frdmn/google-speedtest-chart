75612116590b0a6cc965e261a76463000b7a607b
Scott Macdonald <scottm@bed.link>
2018-06-12 19:51:12 -0400

    Removed unused modules
    refactored code quite a bit
    
    added in ability to create a new sheet for each month
    
    added in function for testing, will be removed

--- speedtest-charts.py
+++ speedtest-charts.py
@@ -1,51 +1,107 @@
 #!/usr/bin/env python3
 
 import os
-import subprocess
-import re
 import datetime
 import pygsheets
 import speedtest
 
+# Set options
+bymonth = True
+
 # Set constants
 DATE = datetime.datetime.now().strftime("%d-%m-%y %H:%M:%S")
+sheetname = datetime.datetime.now().strftime("%b test1 %Y")
+header = [['Date'], ['Download'], ['Upload'], ['Ping']]
+workbookname = os.getenv('SPREADSHEET', 'Speedtest-test3')
+
+# set variable scope
+download = ''
+upload = ''
+ping = ''
+
 
 def get_credentials():
     """Function to check for valid OAuth access tokens."""
     gc = pygsheets.authorize(outh_file="credentials.json")
     return gc
 
+
 def submit_into_spreadsheet(download, upload, ping):
     """Function to submit speedtest result."""
     gc = get_credentials()
 
-    speedtest = gc.open(os.getenv('SPREADSHEET', 'Speedtest'))
-    sheet = speedtest.sheet1
+    try:
+        speedtest = gc.open(workbookname)
+    except pygsheets.SpreadsheetNotFound:
+        speedtest = gc.create(workbookname)
+
+    if not bymonth:
+        sheet = speedtest.sheet1
+    else:
+        try:
+            sheet = speedtest.worksheet('title', sheetname)
+        except pygsheets.WorksheetNotFound:
+            sheet = speedtest.add_worksheet(sheetname)
+
+        head1 = sheet.cell('A1').value
+        print(head1)
+        if head1 != header[1]:
+            sheet.update_cells('A1:D1', header)
+            headrange = sheet.range('A1:D1')
+            headrange.set_text_format('bold', 'true')
 
     data = [DATE, download, upload, ping]
 
     sheet.append_table(values=data)
 
-def main():
-    # Check for proper credentials
-    print("Checking OAuth validity...")
-    credentials = get_credentials()
 
-    # Run speedtest and store output
-    print("Starting speed test...")
+def getresults():
     spdtest = speedtest.Speedtest()
     spdtest.get_best_server()
+<<<<<<< HEAD
     
     download = round(spdtest.download() / 1000 / 1000, 2)
     upload = round(spdtest.upload() / 1000 / 1000, 2)
     ping = round(spdtest.results.ping)
 
     print("Starting speed finished (Download: ", download, ", Upload: ", upload, ", Ping: ", ping, ")")
+=======
+    download = spdtest.download()
+    upload = spdtest.upload()
+    ping = spdtest.results.ping
+
+    return(download, upload, ping)
+
+
+def getresults_test():
+    download = '200'
+    upload = '300'
+    ping = '20'
+
+    return(download, upload, ping)
+
+
+def main():
+    # Check for proper credentials
+    print("Checking OAuth validity...")
+    try:
+        get_credentials()
+    except pygsheets.AuthenticationError:
+        print("Authentication Failed")
+        raise
+
+    # Run speedtest and store output
+    print("Starting speed test...")
+#    getresults()
+    getresults_test()
+    print("Starting speed finished!")
+>>>>>>> Removed unused modules
 
     # Write to spreadsheet
     print("Writing to spreadsheet...")
     submit_into_spreadsheet(download, upload, ping)
     print("Successfuly written to spreadsheet!")
+
 
 if __name__ == "__main__":
     main()
