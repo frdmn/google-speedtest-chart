880c5369800f73e18011142927d1367383f74eb8
Scott Macdonald <scottm@bed.link>
2018-06-17 19:14:29 -0400

    Adding in paramaters for nameing the Worrkbook, naming the Sheeet, and creating a sheet per month

--- speedtest-charts.py
+++ speedtest-charts.py
@@ -1,18 +1,41 @@
 #!/usr/bin/env python3
 
-import os
 import datetime
 import pygsheets
 import speedtest
+import argparse
 
 # Set options
-bymonth = False
+parser = argparse.ArgumentParser(
+    description='Simple Python script to push speedtest results \
+                (using speedtest-cli) to a Google Docs spreadsheet'
+)
+parser.add_argument(
+    "-w, --workbookname", action="store", default="Speedtest", type=str,
+    dest="workbookname",
+    help='Sets the woorkbook name, default is "Speedtest"'
+)
+parser.add_argument(
+    "-s, --sheetname", action="store", default="Sheet1", type=str,
+    dest="sheetname",
+    help='Sets the sheet name if "bymonth" not set, default is "sheet1"'
+)
+parser.add_argument(
+    "-b, --bymonth", action="store_true", default=False,
+    dest="bymonth",
+    help='Creats a new sheet for each month named MMM YYYY (ex: Jun 2018)'
+)
+
+cliarg = parser.parse_args()
 
 # Set constants
 DATE = datetime.datetime.now().strftime("%m/%d/%Y %H:%M:%S")
-sheetname = datetime.datetime.now().strftime("%b %Y")
 header = [['A1', 'B1', 'C1', 'D1'], ['Date', 'Download', 'Upload', 'Ping']]
-workbookname = os.getenv('SPREADSHEET', 'Speedtest')
+
+if cliarg.bymonth:
+    sheetname = datetime.datetime.now().strftime("%b %Y")
+else:
+    sheetname = cliarg.sheetname
 
 # set variable scope
 download = ''
@@ -31,27 +54,24 @@ def submit_into_spreadsheet(download, upload, ping):
     gc = get_credentials()
 
     try:
-        speedtest = gc.open(workbookname)
+        speedtest = gc.open(cliarg.workbookname)
     except pygsheets.SpreadsheetNotFound:
-        speedtest = gc.create(workbookname)
+        speedtest = gc.create(cliarg.workbookname)
 
-    if not bymonth:
-        sheet = speedtest.sheet1
-    else:
-        try:
-            sheet = speedtest.worksheet('title', sheetname)
-        except pygsheets.WorksheetNotFound:
-            sheet = speedtest.add_worksheet(sheetname)
+    try:
+        sheet = speedtest.worksheet('title', sheetname)
+    except pygsheets.WorksheetNotFound:
+        sheet = speedtest.add_worksheet(sheetname)
 
-        headnew = str(sheet.cell('A1').value)
-        headcur = str(header[1][0])
+    headnew = str(sheet.cell('A1').value)
+    headcur = str(header[1][0])
 
-        if headnew != headcur:
-            # create header row
-            for index in range(len(header[0])):
-                head = sheet.cell(header[0][index])
-                head.value = header[1][index]
-                head.update()
+    if headnew != headcur:
+        # create header row
+        for index in range(len(header[0])):
+            head = sheet.cell(header[0][index])
+            head.value = header[1][index]
+            head.update()
 
     data = [DATE, download, upload, ping]
 
